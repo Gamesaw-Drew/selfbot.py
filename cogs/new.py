6fd356bc602a055ed0adaf7719f4535417ab6411
Drew C <drewgamesaw@gmail.com>
2017-10-26 23:01:27 -0400

    xd

--- cogs/new.py
+++ cogs/new.py
@@ -13,6 +13,7 @@ import io
 import aiohttp
 import json
 import os
+import traceback
 
 
 class New:
@@ -33,13 +34,12 @@ class New:
                 font = get_config_value("optional_config", "ascii_font")
                 msg = str(figlet_format(msg.strip(), font=font))
                 if len(msg) > 2000:
-                    await ctx.send(self.bot.bot_prefix + 'Message too long, RIP.')
+                    await ctx.send(self.bot.bot_prefix + 'no fuck off')
                 else:
                     await ctx.message.delete()
                     await ctx.send(self.bot.bot_prefix + '```\n{}\n```'.format(msg))
             else:
-                await ctx.send(
-                               self.bot.bot_prefix + 'Please input text to convert to ascii art. Ex: ``>ascii stuff``')
+                await ctx.send(self.bot.bot_prefix + "put in text noob")
 
     @commands.command(pass_context=True)
     async def textflip(self, ctx, *, msg):
@@ -60,6 +60,89 @@ class New:
         regional_list = [self.regionals[x.lower()] if x.isalnum() or x in ["!", "?"] else x for x in msg]
         regional_output = '\u200b'.join(regional_list)
         await ctx.send(regional_output)
-		
+        
+    @commands.command(pass_context=True)
+    async def heil(self):
+        try:
+            await self.bot.say("卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐")
+        except:
+            await self.bot.say(str("```" + traceback.format_exc() + "```"))
+            
+    @commands.command(pass_context=True, no_pm=False)
+    async def stab(self, ctx):
+        try:
+            if len(ctx.message.mentions) > 1:
+                await self.bot.say("You can only stab one person at a time " + ctx.message.author.mention + "!")
+                pass
+            elif len(ctx.message.mentions) == 0:
+                await self.bot.say("`usage: !stab <user>`")
+                pass
+            else:
+                if ctx.message.mentions[0] == ctx.message.author:
+                    await self.bot.send_message(ctx.message.channel, ':knife: ' + ctx.message.author.mention + ' commited suicide')
+                else:
+                    await self.bot.send_message(ctx.message.channel, '#getstabbed ' + ctx.message.mentions[0].mention + ' :knife:')
+        except:
+            await self.bot.say(str("```" + traceback.format_exc() + "```"))
+            
+    @commands.command(pass_context=True, no_pm=False)
+    async def shoot(self, ctx):
+        try:
+            if len(ctx.message.mentions) > 1:
+                await self.bot.say("You can only police brutality one person at a time " + ctx.message.author.mention + "!")
+                pass
+            elif len(ctx.message.mentions) == 0:
+                await self.bot.say("`usage: !shoot <user>`")
+                pass
+            else:
+                if ctx.message.mentions[0] == ctx.message.author:
+                    await self.bot.send_message(ctx.message.channel, ':gun: ' + ctx.message.author.mention + '\'s gun jammed! UH OH!!!!')
+                else:
+                    await self.bot.send_message(ctx.message.channel, 'Preparing to shoot ' + ctx.message.mentions[0].mention + '...')
+                    await asyncio.sleep(2)
+                    await self.bot.send_message(ctx.message.channel, ctx.message.mentions[0].mention + ' :gun: UH OH! THE GUN JAMMED! UH OH! UH OH!')
+        except:
+            await self.bot.say(str("```" + traceback.format_exc() + "```"))
+            
+    @commands.command(pass_context=True, no_pm=False)
+    async def bomb(self, ctx):
+        try:
+            if len(ctx.message.mentions) > 1:
+                await self.bot.say("You can only isis one person at a time " + ctx.message.author.mention + "!")
+                pass
+            elif len(ctx.message.mentions) == 0:
+                await self.bot.say("`usage: !bomb <user>`")
+                pass
+            else:
+                if ctx.message.mentions[0] == ctx.message.author:
+                    await self.bot.send_message(ctx.message.channel, ':man_with_turban::skin-tone-5: :bomb: ' + ctx.message.author.mention + ' jihaded them\'re selves to death')
+                else:
+                    await self.bot.send_message(ctx.message.channel, 'Transforming ' + ctx.message.mentions[0].mention + ' into a school and then bombing it...\n:man_with_turban::skin-tone-5: :school: :bomb: *allahu akbar*')
+        except:
+            await self.bot.say(str("```" + traceback.format_exc() + "```"))
+            
+    @commands.command(pass_context=True, no_pm=False)
+    async def robloxavatar(self, ctx, roblobabeUserName):
+        getId = requests.get("https://api.roblox.com/users/get-by-username?username=" + roblobabeUserName)
+        idJson = getId.json()
+        userId = idJson['Id']
+        
+        avatarImage = requests.get("http://www.roblox.com/bust-thumbnail/json?userId="+ str(userId) +"&height=180&width=180")
+        avImg = avatarImage.json()
+        await self.bot.send_message(ctx.message.channel, avImg['Url'])
+        
+    @commands.command(pass_context=True, no_pm=False)
+    async def minecraftavatar(self, ctx, minecraftUsername):
+        await self.bot.send_message(ctx.message.channel, "https://mcapi.ca/skin/2d/" + minecraftUsername + "/85/true")
+        
+    @commands.command(pass_context=True, no_pm=False)
+    async def minecraftserver(self, ctx, serverAddress):
+        await self.bot.say("Getting server data, it may take a bit, and sometimes it may not work...")
+        getStatus = requests.get("https://mcapi.ca/query/" + serverAddress + "/info")
+        getPlayerList = requests.get("https://mcapi.ca/query/" + serverAddress + "/list")
+        players = getPlayerList.json()
+        status = getStatus.json()
+        await self.bot.send_message(ctx.message.channel, "```MOTD: " + status['motd'] + "\nPlayers: " + str(status['players']['online']) + "/" + str(status['players']['max']) + "\nPlayers List: " + str(players['Players']['list']) + "```")
+
 def setup(bot):
     bot.add_cog(New(bot))
